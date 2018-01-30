ec6be0824572f1de2dbc3dd3e2bb23235481649c
chisl <info@chisl.io>
2018-01-30 17:22:31 +0100

    Initial commit

---
+++ STLM75.h
@@ -0,0 +1,15 @@
+/*
+ * name:        STLM75
+ * description: Digital temperature sensor and thermal watchdog
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/stlm75.pdf
+ * date:        2018-01-30
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
